f553a29e0898ab8091ba3f14429f341622e28f5b
Yuqing Zhou <zyq@umich.edu>
2024-06-15 14:04:51 -0400

    update code

--- code/bert.py
+++ code/bert.py
@@ -6,66 +6,98 @@ from utils import *
 import wandb
 from transformers import AutoTokenizer, DataCollatorWithPadding, AutoModelForSequenceClassification, TrainingArguments, \
     Trainer, pipeline
-from concept_dataset_process import load_dataset
+# from concept_dataset_process import load_dataset
 from utils import DATASETS_INFO
+from datasets import load_metric
+
 
 def preprocess_function(examples):
     text = examples['text']
-    return tokenizer(text, padding='max_length', truncation=True, max_length = 512)
+    return tokenizer(text, padding='max_length', truncation=True, max_length=512)
 
 
 # seed = 42
-DATASET_NAME =  'yelp' #  'beer' # 'go_emotions' #
+# DATASET_NAME =  'go_emotions' # 'yelp' #  'beer' #
 MODEL_TYPE = 'bert'
-setting_No = 2
-n_exp = 4
-lr= 2e-5
-NUM_EPOCHS = 15 # 20 #
-dataset_types =  ['train', 'test']
-label_shift = DATASETS_INFO[DATASET_NAME] ['label_shift_to_index']
+setting_No = 1
+n_exp = 0
+NUM_EPOCHS = 15  # 20 #
+dataset_types = ['train', 'dev', 'test']
+label_shift = DATASETS_INFO[DATASET_NAME]['label_shift_to_index']
 dataset_dir = DATASETS_INFO[DATASET_NAME]['path']
 num_labels = DATASETS_INFO[DATASET_NAME]['num_labels']
-shortcut_type = 'style' # 'occurrence' # 'concept' #
-shortcut_subtype = 'author' # 'single-word' # 'occurrence' # 'synonym'  # 'category' #
-save_path =  f"../model/{DATASET_NAME}/{MODEL_TYPE}/{shortcut_type}/{shortcut_subtype}_{setting_No}_{n_exp}"
-test_type =  'normal' #
+shortcut_type = SET_SHORTCUT_TYPE  # 'occurrence' # 'style' #  'concept' #
+shortcut_subtype = SET_SHORTCUT_SUBTYPE1  # 'single-word' # 'synonym'  #  'author' #  'occurrence' #  'category' #
+save_path = f"../model/{DATASET_NAME}/{MODEL_TYPE}/{shortcut_type}/{shortcut_subtype}_{setting_No}_{n_exp}"
+test_type = 'normal'  # 'anti-shortcut' #
 model_name = 'bert-base-uncased' if test_type == 'normal' else save_path
 repo_name = f'finetuning-{DATASET_NAME}-{MODEL_TYPE}-{shortcut_subtype}-{shortcut_subtype}'
+lr = BERT_LR[DATASET_NAME][shortcut_subtype]  # 1e-5
+weight_decay = WEIGHT_DECAY[DATASET_NAME][shortcut_subtype]
 
+seed = seeds[n_exp]  # 0
+set_seed(seed)
+
+ori_test_flg = True
+anti_test_flg = True
+normal_test_flg = True
+
+project_name = wandb_project_name(MODEL_TYPE, shortcut_type, shortcut_subtype)
+exp_name = wandb_exp_name(DATASET_NAME, setting_No, n_exp, test_type, lr, weight_decay)
 wandb.init(
-    project= f"Shortcuts-{shortcut_type}-{shortcut_subtype}",
+    # set the wandb project where this run will be logged
+    project=project_name,
+    # f"Shortcuts-{shortcut_type}-{shortcut_subtype}", # "Shortcuts-Concept-Occurrence", # 'Shortcuts-Occurrence-Single-Word', #   'Shortcuts-Occurrence-Class-Word', #   "Shortcuts-Concept-Correlated", # 'Shortcuts-Style-Author', #  "Shortcuts-Occurrence-Synonym", #
+    # track hyperparameters and run metadata
     config={
         "learning_rate": lr,
         "architecture": "bert-base-uncased",
-        "dataset":DATASET_NAME,
+        "dataset": DATASET_NAME,
         "epochs": NUM_EPOCHS,
+        "weight_decay": weight_decay,
     },
-    name = f"{DATASET_NAME}_{setting_No}_{n_exp}_{test_type}"
+    name=exp_name  # f"{DATASET_NAME}_{setting_No}_{n_exp}_{test_type}"
 )
 
 # Load the dataset
 dataset_dict = DatasetDict()
+dataset_path = os.path.join(dataset_dir, shortcut_type + f'/split/{shortcut_subtype}',
+                            shortcut_subtype + str(setting_No))
 for dataset_type in dataset_types:
-    dataset_path = os.path.join(dataset_dir, shortcut_type, shortcut_subtype + str(setting_No))
-    if dataset_type == 'test' and test_type == 'anti-shortcut':
-        file_path = os.path.join(dataset_path, f'{dataset_type}_anti-shortcut.csv')
+    if dataset_type == 'test': #  and test_type == 'anti-shortcut':
+        file_path = os.path.join(dataset_path[:-1] + '1', f'{dataset_type}.csv')
     else:
         file_path = os.path.join(dataset_path, f'{dataset_type}.csv')
     dataset = load_dataset(file_path)
     print(dataset[0])
-    dataset_dict[dataset_type] = dataset.map(lambda example: {'text': example['text'], 'label': int(example['label'] - label_shift)})
+    dataset_dict[dataset_type] = dataset.map(
+        lambda example: {'text': example['text'], 'label': int(example['label'] - label_shift)})
 # print(dataset_dict)
+
+dataset_path = os.path.join(dataset_dir, shortcut_type + f'/split/{shortcut_subtype}', shortcut_subtype + '1')
+anti_test_path = os.path.join(dataset_path, 'test_anti-shortcut.csv')
+dataset = load_dataset(anti_test_path)
+dataset_dict['test_anti-shortcut'] = dataset.map(
+    lambda example: {'text': example['text'], 'label': int(example['label'] - label_shift)})
+
+original_test_path = os.path.join(dataset_dir, shortcut_type, 'test.csv')
+dataset = load_dataset(original_test_path)
+dataset_dict['ori-test'] = dataset.map(
+    lambda example: {'text': example['text'], 'label': int(example['label'] - label_shift)})
 
 # Load the model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
+# tokenizer.pad_token = tokenizer.eos_token
+# tokenizer.padding_side = "right"
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
 
 # Prepare the dataset
 tokenized_datasets = dataset_dict.map(preprocess_function, batched=True)
-tokenized_train = tokenized_datasets["train"].shuffle()#(seed=seed)#.select(range(1000))
+tokenized_train = tokenized_datasets["train"].shuffle(seed=seed)  # .select(range(1000))
+tokenized_val = tokenized_datasets["dev"]
 tokenized_test = tokenized_datasets["test"]
+tokenized_test_anti = tokenized_datasets["test_anti-shortcut"]
+tokenized_test_ori = tokenized_datasets['ori-test']
 # print(tokenized_datasets['train'][0])
 # print(tokenized_train[0])
 
@@ -77,8 +109,12 @@ training_args = TrainingArguments(
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     num_train_epochs=NUM_EPOCHS,
-    weight_decay=0.01,
+    weight_decay=weight_decay,  # 0.01,
     save_strategy="epoch",
+    evaluation_strategy='epoch',
+    load_best_model_at_end=True,
+    metric_for_best_model='eval_loss',
+    greater_is_better=False,
     # push_to_hub=False,
 )
 
@@ -86,7 +122,7 @@ trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=tokenized_train,
-    eval_dataset=tokenized_test,
+    eval_dataset=tokenized_val,
     tokenizer=tokenizer,
     data_collator=data_collator,
     compute_metrics=compute_metrics,
@@ -101,12 +137,59 @@ if test_type == 'normal':
     tokenizer.save_pretrained(save_path)
 
 trainer.evaluate()
-
+load_accuracy = load_metric("accuracy")
+load_f1 = load_metric("f1")
 # Save the prediction
-result = trainer.predict(tokenized_test)
-predictions = np.argmax(result.predictions, axis=-1)
-data = pd.read_csv(file_path)
-data[f'pred_{n_exp}'] = predictions + label_shift
-data.to_csv(file_path, index=False)
+# Test
+if normal_test_flg == True:
+    result = trainer.predict(tokenized_test)
+    predictions = np.argmax(result.predictions, axis=-1)
+    labels = result.label_ids
 
+    accuracy = load_accuracy.compute(predictions=predictions, references=labels)["accuracy"]
+    macro_f1 = load_f1.compute(predictions=predictions, references=labels, average='macro')["f1"]
+
+    print(f"Test Accuracy: {accuracy}")
+    print(f"Test Macro F1: {macro_f1}")
+    wandb.log({"Test Accuracy": accuracy, "Test Macro F1": macro_f1})
+
+    data = pd.read_csv(anti_test_path)
+    data[f'pred_{n_exp}'] = predictions + label_shift
+    data.to_csv(anti_test_path, index=False)
+
+# Anti-Test
+if anti_test_flg == True:
+    result = trainer.predict(tokenized_test_anti)
+    predictions = np.argmax(result.predictions, axis=-1)
+    labels = result.label_ids
+
+    anti_accuracy = load_accuracy.compute(predictions=predictions, references=labels)["accuracy"]
+    anti_macro_f1 = load_f1.compute(predictions=predictions, references=labels, average='macro')["f1"]
+
+    print(f"Anti-Test Accuracy: {anti_accuracy}")
+    print(f"Anti-Test Macro F1: {anti_macro_f1}")
+    wandb.log({"Anti-Test Accuracy": anti_accuracy, "Anti-Test Macro F1": anti_macro_f1})
+
+    data = pd.read_csv(anti_test_path)
+    data[f'pred_{n_exp}'] = predictions + label_shift
+    data.to_csv(anti_test_path, index=False)
+
+# Original Test
+if ori_test_flg == True:
+    result = trainer.predict(tokenized_test_ori)
+    predictions = np.argmax(result.predictions, axis=-1)
+    labels = result.label_ids
+
+    original_test_accuracy = load_accuracy.compute(predictions=predictions, references=labels)["accuracy"]
+    original_test_macro_f1 = load_f1.compute(predictions=predictions, references=labels, average='macro')["f1"]
+
+    print(f"Original Test Accuracy: {original_test_accuracy}")
+    print(f"Original Test Macro F1: {original_test_macro_f1}")
+    wandb.log({"Original Test Accuracy": original_test_accuracy, "Original Test Macro F1": original_test_macro_f1})
+
+    data = pd.read_csv(original_test_path)
+    data[f'{shortcut_subtype}_{setting_No}_{n_exp}_pred'] = predictions + label_shift
+    data.to_csv(original_test_path, index=False)
 # trainer.push_to_hub()
+
+wandb.finish()
--- code/utils.py
+++ code/utils.py
@@ -5,6 +5,33 @@ import torch
 import matplotlib.pyplot as plt
 from collections import Counter
 from transformers import Trainer
+import random
+import os
+from huggingface_hub import login
+from datasets import Dataset, DatasetDict
+
+# DATASETS_INFO = {
+#     'label_shift_to_index': {
+#         'go_emotions': 0,
+#         'yelp': 1,
+#         'beer': 2
+#     },
+#     'num_labels': {
+#         'go_emotions': 4,
+#         'yelp': 5,
+#         'beer': 4 # 9
+#     },
+#     'path': {
+#         'go_emotions': '../Dataset/go_emotions/',
+#         'yelp': '../Dataset/yelp_review_full_csv/',
+#         'beer': "../Dataset/beer_new/target/",
+#     },
+#     'instruction': {
+#         'go_emotions': f'Categorize the text into one of the 4 emotions: neutral, amusement, joy and excitement. Use [0, 1, 2, 3] to represent the four emotions:\n\n0: neutral\n\n1: amusement\n\n2: joy\n\n3: excitement. Follow the format: \nText:\n [Text] \nCategory: [NUMBER]',
+#         'yelp': f'Select one rating from [1, 2, 3, 4, 5] according to this review, where 1 represents the lowest and 5 represents the highest satisfaction level, and follow the format: \nReview:\n [REVIEW] \nRating: [NUMBER]',
+#         'beer': "",
+#     }
+# }
 
 DATASETS_INFO = {
     'go_emotions': {
@@ -15,6 +42,7 @@ DATASETS_INFO = {
         'INSTRUCTION_KEY': f'Pick the most suitable one from the 4 emotions for the text: neutral, amusement, joy and excitement. Use [0, 1, 2, 3] to represent the four emotions:\n\n0: neutral\n\n1: amusement\n\n2: joy\n\n3: excitement. Follow the format: \nText:\n [Text] \n### Category: [NUMBER]',
         'INPUT_KEY': 'Text:',
         'RESPONSE_KEY': "### Category: [",
+        'training_dataset_size': 1120,
     },
     'yelp': {
         'label_shift_to_index': 1,
@@ -23,6 +51,7 @@ DATASETS_INFO = {
         'INSTRUCTION_KEY': f'Select one rating from [1, 2, 3, 4, 5] according to this review, where 1 represents the lowest and 5 represents the highest satisfaction level. Follow the format: \nReview:\n [REVIEW] \n### Rating: [NUMBER]',
         'INPUT_KEY': 'Review:',
         'RESPONSE_KEY': "### Rating: [",
+        'training_dataset_size': 8900,
     },
 
     'beer': {
@@ -32,9 +61,51 @@ DATASETS_INFO = {
         'INSTRUCTION_KEY': f'Select one rating from [2, 3, 4, 5] according to this review, where 4 represents the lowest and 10 represents the highest satisfaction level. Follow the format: \nReview:\n [REVIEW] \n### Rating: [NUMBER]',
         'INPUT_KEY': 'Review:',
         'RESPONSE_KEY': "### Rating: [",
+        'training_dataset_size': 1999,
     }
 }
 
+def load_dataset(file_path):
+    if file_path.endswith('.xlsx'):
+        # Load my training dataset
+        dataset = pd.read_excel(file_path, usecols=['label', 'text'])
+    elif file_path.endswith('.csv'):
+        dataset = pd.read_csv(file_path, usecols=['label', 'text'])
+
+    if 'beer' in file_path:
+        # Mapping ratings to integer labels
+        dataset['label'] = (dataset['label'] / 2 * 10).astype(int)
+    dataset = Dataset.from_pandas(dataset)
+    return dataset
+
+def dataset_read(dataset_dir, shortcut_type, shortcut_subtype, setting_No, label_shift):
+    dataset_dict = DatasetDict()
+    dataset_path = os.path.join(dataset_dir, shortcut_type + f'/split/{shortcut_subtype}', shortcut_subtype + str(setting_No))
+    for dataset_type in dataset_types:
+        if dataset_type == 'test':
+            file_path = os.path.join(dataset_path[:-1] + '1', f'{dataset_type}.csv')
+        else:
+            file_path = os.path.join(dataset_path, f'{dataset_type}.csv')
+        dataset = load_dataset(file_path)
+        print(dataset[0])
+        dataset_dict[dataset_type] = dataset.map(
+            lambda example: {'text': example['text'], 'label': int(example['label'] - label_shift)})
+    # print(dataset_dict)
+
+    dataset_path = os.path.join(dataset_dir, shortcut_type + f'/split/{shortcut_subtype}', shortcut_subtype + '1')
+    anti_test_path = os.path.join(dataset_path, 'test_anti-shortcut.csv')
+    dataset = load_dataset(anti_test_path)
+    dataset_dict['test_anti-shortcut'] = dataset.map(
+        lambda example: {'text': example['text'], 'label': int(example['label'] - label_shift)})
+
+    if 'beer' in dataset_dir:
+        original_test_path = os.path.join(dataset_dir, shortcut_type, 'test.xlsx')
+    else:
+        original_test_path = os.path.join(dataset_dir, shortcut_type, 'test.csv')
+    dataset = load_dataset(original_test_path)
+    dataset_dict['ori-test'] = dataset.map(lambda example: {'text': example['text'], 'label': int(example['label'] - label_shift)})
+
+    return dataset_dict
 
 def compute_metrics(eval_pred):
     load_accuracy = load_metric("accuracy")
@@ -52,9 +123,29 @@ def pred(row, generate_text, data_instruction, data_input_key, data_response_key
     text = row["text"]
     label = row["label"]
 
+    # Initialize static strings for the prompt template
+    INTRO_BLURB = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+    INSTRUCTION_KEY = "### Instruction:"
+    # INPUT_KEY = "Review:"  # "Text:" #"Input:"
+    # RESPONSE_KEY = "### Rating: ["  # "### Category: [" # "### Response:"
+    END_KEY = "### End"
+
+    # Combine a prompt with the static strings
+    blurb = f"{INTRO_BLURB}"
+    instruction = f"{INSTRUCTION_KEY}\n{data_instruction}"
     input_context = f"{data_input_key}\n{text}" if text else None
-    parts = [part for part in [f'### Instruction:\n' + data_instruction, input_context, data_response_key] if part]
+    response = f"{data_response_key}"
+    # end = f"{END_KEY}"
+
+    # Create a list of prompt template elements
+    parts = [part for part in [blurb, instruction, input_context, response] if part]
+
+    # Join prompt template elements into a single string to create the prompt template
     formatted_prompt = "\n\n".join(parts)
+
+    # input_context = f"{data_input_key}\n{text}" if text else None
+    # parts = [part for part in [f'### Instruction:\n' + data_instruction, input_context, data_response_key] if part]
+    # formatted_prompt = "\n\n".join(parts)
 
     prompt_len = len(formatted_prompt)
     res = generate_text(formatted_prompt)
@@ -97,3 +188,198 @@ def frequency_show(dataset):
     plt.grid(True)
     plt.show()
 
+
+def set_seed(seed_value=42):
+    random.seed(seed_value)  # Python random module
+    np.random.seed(seed_value)  # Numpy module
+    torch.manual_seed(seed_value)  # PyTorch
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)
+
+
+def wandb_project_name(model_type, shortcut_type, shortcut_subtype):
+    # project_name=f"Shortcuts-{model_type}-Hyperparameter-{shortcut_type}-{shortcut_subtype}"
+    project_name = f"Shortcuts-{model_type}-{shortcut_type}-{shortcut_subtype}"
+    return project_name
+
+
+def wandb_exp_name(DATASET_NAME, setting_No, n_exp, test_type, lr, weight_decay=0):
+    name = f"{DATASET_NAME}_{setting_No}_{n_exp}_{test_type}"
+    # name = f"{DATASET_NAME}_{setting_No}_{n_exp}_{test_type}_{lr}_{weight_decay}"
+    return name
+
+def wandb_exp_name_llm(DATASET_NAME, setting_No, n_exp, lr, r, alpha):
+    # name = f"{DATASET_NAME}_{setting_No}_{n_exp}"
+    name = f"{DATASET_NAME}_{setting_No}_{n_exp}_{lr}_{r}_{alpha}"
+    return name
+
+def llm_output_dir(model_name, model_size, dataset_name, shortcut_type, shortcut_subtype, setting_No, n_exp):
+    output_dir = os.path.join("./results", f"{model_name}_{model_size}b_{dataset_name}_{shortcut_type}_{shortcut_subtype}_{setting_No}_{n_exp}")
+    return output_dir
+
+
+
+
+login(token='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')
+DATASET_NAME = 'yelp' #  'beer' #  'go_emotions'  #
+
+seeds = [0, 1, 2, 10, 42]
+# seeds = [1, 1, 1, 1, 1]
+
+dataset_types = ['train', 'test', 'dev']
+
+SET_SHORTCUT_TYPE = 'occurrence'  # 'concept'  #  'style' #
+SET_SHORTCUT_SUBTYPE1 =  'single-word'  #  'occurrence'  # 'category' # 'register' #       'single-word'  #             'tone' #
+SET_SHORTCUT_SUBTYPE2 =  'single-word'  #  'occurrence'  #  'category' #'author' #   'register' #  'correlation' #  'occurrence'  # 'correlation' # 'occurrence'  # 'author' #   'occurrence'  # 'synonym' #   'author' # 'register' #  'category' # 'synonym' # 'single-word'  #  'single-word'  # 'synonym'  #   'synonym' #'synonym'
+############ BERT ############
+# BERT_LR= {'go_emotions': {'single-word': 1e-3, 'synonym': 1e-2 }, 'yelp': {'single-word': 2e-5, 'synonym': 1e-5}}
+# WEIGHT_DECAY= {'go_emotions': {'single-word': 0, 'synonym': 0.1}, 'yelp': {'single-word': 0.01, 'synonym': 0}}
+BERT_LR = {'go_emotions': {'single-word': 1e-5, 'synonym': 2e-5}, 'yelp': {'single-word': 2e-5, 'synonym': 1e-5}}
+WEIGHT_DECAY = {'go_emotions': {'single-word': 0.1, 'synonym': 0.1}, 'yelp': {'single-word': 0.01, 'synonym': 0}}
+test_type = 'normal' # 'test' #  'anti-shortcut' #
+
+############ Llama ############
+# LLAMA2_7B_LR = {'go_emotions': {'single-word': 2e-4, 'synonym': 2e-4 }, 'yelp': {'single-word': 2e-5, 'synonym': 2e-3}}
+# LLAMA2_7B_LORA_R = {'go_emotions': {'single-word': 256, 'synonym': 128}, 'yelp': {'single-word': 256, 'synonym': 64}}
+# LLAMA2_7B_LORA_ALPHA = {'go_emotions': {'single-word': 32, 'synonym': 16 }, 'yelp': {'single-word': 8, 'synonym': 16}}
+
+# LLAMA2_13B_LR = {'go_emotions': {'single-word': 2e-3, 'synonym': 2e-3 }, 'yelp': {'single-word': 0, 'synonym': 0}}
+# LLAMA2_13B_LORA_R = {'go_emotions': {'single-word': 64, 'synonym': 64}, 'yelp': {'single-word': 0, 'synonym': 0}}
+# LLAMA2_13B_LORA_ALPHA = {'go_emotions': {'single-word': 8, 'synonym': 8}, 'yelp': {'single-word': 0, 'synonym': 0}}
+
+# Hyperparameter = {
+#     'Llama-2-7B': {
+#         'lr' : {
+#             'go_emotions': {'single-word': 2e-4, 'synonym': 2e-4 },
+#             'yelp': {'single-word': 2e-5, 'synonym': 2e-3}
+#         },
+#         'lora_r': {
+#             'go_emotions': {'single-word': 256, 'synonym': 128},
+#             'yelp': {'single-word': 256, 'synonym': 64}
+#         },
+#         'lora_alpha': {
+#             'go_emotions': {'single-word': 32, 'synonym': 16 },
+#             'yelp': {'single-word': 8, 'synonym': 16}
+#         },
+#     },
+#     'Llama-2-13B': {
+#         'lr' : {
+#             'go_emotions': {'single-word': 2e-3, 'synonym': 2e-3 },
+#             'yelp': {'single-word': 0, 'synonym': 0}
+#         },
+#         'lora_r': {
+#             'go_emotions': {'single-word': 64, 'synonym': 64},
+#             'yelp': {'single-word': 0, 'synonym': 0}
+#         },
+#         'lora_alpha': {
+#             'go_emotions': {'single-word': 8, 'synonym': 8},
+#             'yelp': {'single-word': 0, 'synonym': 0}
+#         },
+#     }
+# }
+
+Hyperparameter = {
+    'go_emotions':{
+        'occurrence':{
+            'single-word':{
+                'Llama-2-7B':{'lr': 2e-4, 'lora_r': 256, 'lora_alpha': 32},
+                'Llama-2-13B': {'lr': 2e-3, 'lora_r': 64, 'lora_alpha': 8},
+            },
+            'synonym':{
+                'Llama-2-7B':{'lr': 2e-4, 'lora_r': 128, 'lora_alpha': 16},
+                'Llama-2-13B': {'lr': 2e-3, 'lora_r': 64, 'lora_alpha': 8},
+            },
+            'category':{
+                'Llama-2-7B':{'lr': 2e-4, 'lora_r': 256, 'lora_alpha': 16},
+                'Llama-2-13B': {'lr': 0, 'lora_r': 0, 'lora_alpha': 0},
+            },
+        },
+        'style':{
+            'author':{
+                'Llama-2-7B':{'lr': 2e-4, 'lora_r': 256, 'lora_alpha': 16},
+                'Llama-2-13B': {'lr': 0, 'lora_r': 0, 'lora_alpha': 0},
+            },
+            'register':{
+                'Llama-2-7B':{'lr': 2e-4, 'lora_r': 64, 'lora_alpha': 16},
+                'Llama-2-13B': {'lr': 0, 'lora_r': 0, 'lora_alpha': 0},
+            },
+        },
+    },
+    'yelp': {
+        'occurrence':{
+            'single-word':{
+                # 'Llama-2-7B':{'lr': 2e-5, 'lora_r': 256, 'lora_alpha': 8},
+                # 'Llama-2-7B':{'lr': 2e-3, 'lora_r': 64, 'lora_alpha': 16},
+                'Llama-2-7B':{'lr': 2e-4, 'lora_r': 64, 'lora_alpha': 8},
+                'Llama-2-13B': {'lr': 2e-3, 'lora_r': 128, 'lora_alpha': 16},
+            },
+            'synonym':{
+                'Llama-2-7B':{'lr': 2e-3, 'lora_r': 64, 'lora_alpha': 16},
+                'Llama-2-13B': {'lr': 2e-3, 'lora_r': 64, 'lora_alpha': 16},
+            },
+            'category':{
+                'Llama-2-7B':{'lr': 2e-4, 'lora_r': 64, 'lora_alpha': 8},
+                'Llama-2-13B': {'lr': 0, 'lora_r': 0, 'lora_alpha': 0},
+            },
+        },
+        'style':{
+            'author':{
+                # 'Llama-2-7B':{'lr': 2e-3, 'lora_r': 128, 'lora_alpha': 8},
+                'Llama-2-7B':{'lr': 2e-5, 'lora_r': 128, 'lora_alpha': 8},
+                'Llama-2-13B': {'lr': 0, 'lora_r': 0, 'lora_alpha': 0},
+            },
+            'register':{
+                'Llama-2-7B':{'lr': 2e-5, 'lora_r': 128, 'lora_alpha': 8},
+                'Llama-2-13B': {'lr': 0, 'lora_r': 0, 'lora_alpha': 0},
+            },
+        },
+    },
+    'beer': {
+        'concept':{
+            'occurrence':{
+                # 'Llama-2-7B':{'lr': 2e-4, 'lora_r': 256, 'lora_alpha': 8},
+                'Llama-2-7B':{'lr': 2e-3, 'lora_r': 128, 'lora_alpha': 16},
+                'Llama-2-13B': {'lr': 0, 'lora_r': 0, 'lora_alpha': 0},
+            },
+            'correlation':{
+                'Llama-2-7B':{'lr': 2e-4, 'lora_r': 64, 'lora_alpha': 16},
+                'Llama-2-13B': {'lr': 0, 'lora_r': 0, 'lora_alpha': 0},
+            },
+        },
+    },
+}
+
+
+MODEL_VERSION = 2
+MODEL_SIZE = 7
+BATCH_SIZE=16
+LOAD_LOCAL_MODEL = False
+gradient_accumulation_steps = 2
+num_epochs = 5 #  7 # 10 #
+local_files_only = False
+train_flg = True
+MAX_NEW_TOKENS = 5
+hyperparameter_search_flg =  False # True #
+
+TEMPERATURE= 0.0000
+
+def llm_hyperparameter_config(model_name, dataset_name, shortcut_type, shortcut_subtype):
+    lr = Hyperparameter[dataset_name][shortcut_type][shortcut_subtype][model_name]['lr'] # LLAMA2_7B_LR[dataset_name][shortcut_subtype]
+    lora_r = Hyperparameter[dataset_name][shortcut_type][shortcut_subtype][model_name]['lora_r'] #LLAMA2_7B_LORA_R[dataset_name][shortcut_subtype]
+    lora_alpha = Hyperparameter[dataset_name][shortcut_type][shortcut_subtype][model_name]['lora_alpha'] #LLAMA2_7B_LORA_ALPHA[dataset_name][shortcut_subtype]
+
+    return lr, lora_r, lora_alpha
+
+# AFR
+ori_test_flg = True
+anti_test_flg = True
+normal_test_flg = True
+
+load_accuracy = load_metric("accuracy")
+load_f1 = load_metric("f1")
+stage_1_stop_threshold = 1e-5
+reweighting_ratio = 0.2
+num_epochs_afr0 = 20
+num_epochs_afr1 = 20
+LAMBDA_ = 0.1 # 0.01 # 0.5 # 1
